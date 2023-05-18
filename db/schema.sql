d8e64ee084efe6f067e507f8172db328c8a08e10
mirzabaig <baig.zee@hotmail.com>
2023-05-19 07:37:55 +0930

    deployed to render already

--- db/schema.sql
+++ db/schema.sql
@@ -10,8 +10,12 @@ CREATE TABLE goals (
 
 INSERT INTO goals (goal_text, goal_image_url)
 VALUES 
-('Gain Muscles', 'https://infinitefitnesspro.com/wp-content/uploads/2021/02/How-Long-Does-It-Take-To-Build-Muscle.jpg')
-('Loose Weight', 'https://www.pristyncare.com/blog/wp-content/uploads/2019/08/9-Simple-Tips-and-Diet-Plan-That-Will-Help-You-Lose-Weight-In-Just-30-Days.jpg')
+('Gain Muscles', 'https://infinitefitnesspro.com/wp-content/uploads/2021/02/How-Long-Does-It-Take-To-Build-Muscle.jpg');
+INSERT INTO goals (goal_text, goal_image_url)
+VALUES 
+('Loose Weight', 'https://www.pristyncare.com/blog/wp-content/uploads/2019/08/9-Simple-Tips-and-Diet-Plan-That-Will-Help-You-Lose-Weight-In-Just-30-Days.jpg');
+INSERT INTO goals (goal_text, goal_image_url)
+VALUES 
 ('Get Active', 'https://www.alcoa.ca/wp-content/uploads/2018/08/Active-lifestyle-communities-in-Canada-880x660.jpg');
 
 
@@ -29,8 +33,12 @@ CREATE TABLE exercises (
 
 INSERT INTO exercises (day, plan, weight, fasting, diet, image, change, user_id)
 VALUES 
-('Monday', 'Arms', '100 kgs', 'Yes 2 hours', 'Low Carb', 'https://i.postimg.cc/wTV4p3Pq/abeef7a408652305a6ff4af52c09fa9e.jpg', 'Lost 2 kgs', 0)
-('Tuesday', 'Legs', '99 kgs', 'Yes 3 hours', 'Low Carb', 'https://i.postimg.cc/yx777ZkT/legs.jpg', 'Lost 3 kgs', 0)
+('Monday', 'Arms', '100 kgs', 'Yes 2 hours', 'Low Carb', 'https://i.postimg.cc/wTV4p3Pq/abeef7a408652305a6ff4af52c09fa9e.jpg', 'Lost 2 kgs', 0);
+INSERT INTO exercises (day, plan, weight, fasting, diet, image, change, user_id)
+VALUES 
+('Tuesday', 'Legs', '99 kgs', 'Yes 3 hours', 'Low Carb', 'https://i.postimg.cc/yx777ZkT/legs.jpg', 'Lost 3 kgs', 0);
+INSERT INTO exercises (day, plan, weight, fasting, diet, image, change, user_id)
+VALUES 
 ('Wednesday', 'Back', '98 kgs', 'Yes 1 hour', 'No Carb', 'https://i.postimg.cc/k5pJwNTj/back.jpg', 'Lost 2 kgs', 0);
 
 CREATE TABLE legs (
@@ -47,8 +55,12 @@ CREATE TABLE legs (
 
 INSERT INTO legs (day, plan, weight, fasting, diet, image, change, user_id)
 VALUES 
-('Monday', 'Running', '180 kgs', 'Yes 5 hours', 'No Carb', 'https://www.printablee.com/postpic/2013/02/free-printable-ball-exercise-chart_260072.jpg', 'Lost 1 kg', 0)
-('Tuesday', 'Cardio', '178 kgs', 'Yes 3 hours', 'Low Carb', 'https://www.thefitindian.com/blog/wp-content/uploads/2021/12/Beginners-exercises-to-weight-loss.jpg', 'Lost 3 kgs', 0)
+('Monday', 'Running', '180 kgs', 'Yes 5 hours', 'No Carb', 'https://www.printablee.com/postpic/2013/02/free-printable-ball-exercise-chart_260072.jpg', 'Lost 1 kg', 0);
+INSERT INTO legs (day, plan, weight, fasting, diet, image, change, user_id)
+VALUES 
+('Tuesday', 'Cardio', '178 kgs', 'Yes 3 hours', 'Low Carb', 'https://www.thefitindian.com/blog/wp-content/uploads/2021/12/Beginners-exercises-to-weight-loss.jpg', 'Lost 3 kgs', 0);
+INSERT INTO legs (day, plan, weight, fasting, diet, image, change, user_id)
+VALUES 
 ('Wednesday', 'Pushups', '172 kgs', 'Yes 2 hour', 'No Carb', 'https://i.pinimg.com/564x/da/d3/33/dad333aa04bc3c0b697a761a7ceb395a.jpg', 'Lost 9 kgss', 0);
 
 CREATE TABLE shoulders (
@@ -65,8 +77,12 @@ CREATE TABLE shoulders (
 
 INSERT INTO shoulders (day, plan, weight, fasting, diet, image, change, user_id)
 VALUES 
-('Monday', 'Stretching', '98 kgs', 'Yes 2 hours', 'Low Carb', 'https://offgridweb.com/wp-content/uploads/2016/06/Stretching-exercises-2-1024x724.jpg', 'Lost 2 kgs', 0)
-('Tuesday', 'Yoga', '98 kgs', 'Yes 3 hours', 'No Carb', 'https://veritas.widen.net/content/1cmlnjmhpu/webp/Stretching-Pinterest-v5.webp?use=idsla&crop=0&k=&color=&retina=false&u=at8tiu&w=658', 'No Change', 0)
+('Monday', 'Stretching', '98 kgs', 'Yes 2 hours', 'Low Carb', 'https://offgridweb.com/wp-content/uploads/2016/06/Stretching-exercises-2-1024x724.jpg', 'Lost 2 kgs', 0);
+INSERT INTO shoulders (day, plan, weight, fasting, diet, image, change, user_id)
+VALUES
+('Tuesday', 'Yoga', '98 kgs', 'Yes 3 hours', 'No Carb', 'https://veritas.widen.net/content/1cmlnjmhpu/webp/Stretching-Pinterest-v5.webp?use=idsla&crop=0&k=&color=&retina=false&u=at8tiu&w=658', 'No Change', 0);
+INSERT INTO shoulders (day, plan, weight, fasting, diet, image, change, user_id)
+VALUES
 ('Wednesday', 'PE', '97 kgs', 'Yes 1 hour', 'Low Carb', 'https://en.pimg.jp/092/077/314/1/92077314.jpg', 'Lost 3 kgs', 0);
 
 
